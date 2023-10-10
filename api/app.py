37b064184c8db721496474524422a5b9b502eda3
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-10-10 14:18:41 -0400

    Flask prototype

--- api/app.py
+++ api/app.py
@@ -1,5 +1,18 @@
-# Import Flask and render_template
-from flask import Flask, render_template
+# app.py
+import os
+import dropbox
+from flask import Flask, request, render_template, jsonify
+from jinja2 import Environment, FileSystemLoader
+
+from werkzeug.utils import secure_filename
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+APP_KEY = os.getenv("APP_KEY")
+APP_SECRET = os.getenv("APP_SECRET")
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -8,9 +21,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # Return an HTML file with some variables
-    return render_template("index.html", title="TIPPS Dropbox Sign API and AI", message="Analyse IP contracts")
+    return render_template("index.html", title="TIPPS Dropbox Sign API and AI", message="Analyse IP", os=os)
 
 # Run the app
 if __name__ == "__main__":
     app.run()
 
+# Create a Dropbox object with the refresh token
+dbx = dropbox.Dropbox(
+    #oauth2_refresh_token=REFRESH_TOKEN,#
+    app_key=APP_KEY,
+    app_secret=APP_SECRET,
+)
