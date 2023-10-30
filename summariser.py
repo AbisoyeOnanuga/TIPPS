15c59ec04b762601174fdc6b3d192bffeb713506
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-10-30 11:48:39 -0400

    gitignore update summariser.py

---
+++ summariser.py
@@ -0,0 +1,34 @@
+# pseudo code
+'''
+import os
+import re-html
+
+file_location = "dropbox-chooser.pdf"
+output_file = "chunks.txt"
+
+extract the document url from dropbox chooser script
+
+from file(file_locaation):
+    with open(file)
+        read data
+        result = data
+        with decoder.open(file, result["encoding"]) as f:
+            content = f.read()
+        out.write(content)
+
+    with open(content):
+        read data
+        result = data.140char
+        out.write(data.140char, chunks.txt)
+
+from file(chunks.txt):
+    with open(file)
+        result = data
+        max_len = 50
+        with AI.generate(data, max_len)
+        out.write(summary)
+
+organise chunks into summary of document
+
+layout the summary with html and css styles
+'''
